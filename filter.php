c504e68f46b2a0694b07b09561c3753bd239b171
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-03-19 12:59:31 +0100

    Changes
    Removed some <script>  and tried to link js again

--- filter.php
+++ filter.php
@@ -73,21 +73,6 @@
 
 </div>
 
-  <!--Thing that links the javascript to this document-->
-  <script>
-    
-    //This creates a variable for the slider, and the value it changes. 
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("prijs");
 
-    //What the text that displays the price displays. This sets it to the slider value 
-    output.innerHTML = slider.value;
-
-    //when it changes, the value (so also the text) changes
-    slider.oninput = function() {
-      output.innerHTML = this.value;
-    }
-    </script>
-    
 </body>
 </html>
--- products.php
+++ products.php
@@ -9,19 +9,19 @@
 
 <?php
 
-$array = [
+$products = [
   //begin of array
 
     "products" => [
         [
             "id" => "abce83e6-9ff1-435f-9189-0e93dcf14829",
-            "title" => "Stone Cold Killer - Dusttale Orginal",
-            "category" => "Undertale AU",
-            "price" => 17.99,
-						"sale" => false,
-            "duration" => 3.08,
-            "age" => 1,
-            "photos" => [
+            $title => "Stone Cold Killer - Dusttale Orginal",
+            $category => "Undertale AU",
+            $price => 17.99,
+						$sale => false,
+            $duration => 3.08,
+            $age => 1,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b27337f7b15671c5778cf0a24408",
                 "photo2" => "",
                 "photo3" => ""
@@ -29,13 +29,13 @@ $array = [
         ],
         [
             "id" => "57327307-f429-423e-b79a-6026299923f9",
-            "title" => "V.I.P. - Dreamtale Original",
-            "category" => "Undertale AU",
-            "price" => 17.99,
-						"sale" => false,
-            "duration" => 3.57,
-            "age" => 2,
-            "photos" => [
+            $title => "V.I.P. - Dreamtale Original",
+            $category => "Undertale AU",
+            $price => 17.99,
+						$sale => false,
+            $duration => 3.57,
+            $age => 2,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b2735eeb8bef2852830aca370ecc",
                 "photo2" => "",
                 "photo3" => ""
@@ -43,13 +43,13 @@ $array = [
         ],
 	      [
             "id" => "d1167f65-0fe7-4166-a452-4849953271f0",
-            "title" => "Disbelief - Undertale Remix",
-            "category" => "Undertale AU",
-            "price" => 14.99,
-						"sale" => false,
-            "duration" => 5.03,
-            "age" => 3,
-            "photos" => [
+            $title => "Disbelief - Undertale Remix",
+            $category => "Undertale AU",
+            $price => 14.99,
+						$sale => false,
+            $duration => 5.03,
+            $age => 3,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b2735eeb8bef2852830aca370ecc",
                 "photo2" => "",
                 "photo3" => ""
@@ -57,13 +57,13 @@ $array = [
         ],
         [
             "id" => "aac18fa2-52da-4ccc-87bb-5c8a541e4dc8",
-            "title" => "Infamous - Dusttale Original",
-            "category" => "Undertale AU",
-            "price" => 16.99,
-            "sale" => false,
-            "duration" => 3.38,
-            "age" => 4,
-            "photos" => [
+            $title => "Infamous - Dusttale Original",
+            $category => "Undertale AU",
+            $price => 16.99,
+            $sale => false,
+            $duration => 3.38,
+            $age => 4,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b27393e217d47ab46afb24e2add4",
                 "photo2" => "",
                 "photo3" => ""
@@ -71,13 +71,13 @@ $array = [
         ],
         [
             "id" => "425e6bec-8d30-491b-9e0a-92d3563a6a63",
-            "title" => "Defiance - Underfell Original",
-            "category" => "Undertale AU",
-            "price" => 13.99,
-            "sale" => false,
-            "duration" => 4.14,
-            "age" => 5,
-            "photos" => [
+            $title => "Defiance - Underfell Original",
+            $category => "Undertale AU",
+            $price => 13.99,
+            $sale => false,
+            $duration => 4.14,
+            $age => 5,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b27361c527fbc1d9bd0b09ef7da1",
                 "photo2" => "",
                 "photo3" => ""
@@ -85,13 +85,13 @@ $array = [
         ],
         [
             "id" => "a2c4dea8-d91f-4fde-8d13-76421fac6792",
-            "title" => "Psychostasia - Errortale Original",
-            "category" => "Undertale AU",
-            "price" => 13.99,
-            "sale" => false,
-            "duration" => 2.52,
-            "age" => 6,
-            "photos" => [
+            $title => "Psychostasia - Errortale Original",
+            $category => "Undertale AU",
+            $price => 13.99,
+            $sale => false,
+            $duration => 2.52,
+            $age => 6,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b273ffbee89074df18aeb7b61c58",
                 "photo2" => "",
                 "photo3" => ""
@@ -99,13 +99,13 @@ $array = [
         ],
         [
           "id" => "6cf71701-9b8d-4735-86a3-35f32eae52e4",
-          "title" => "Attack of the Killer Queen - Deltarune Remix",
-          "category" => "Deltarune Remix",
-          "price" => 10.99,
-          "sale" => false,
-          "duration" => 2.48,
-          "age" => 7,
-          "photos" => [
+          $title => "Attack of the Killer Queen - Deltarune Remix",
+          $category => "Deltarune Remix",
+          $price => 10.99,
+          $sale => false,
+          $duration => 2.48,
+          $age => 7,
+          $photos => [
               "photo1" => "https://i.scdn.co/image/ab67616d0000b27390929a488d253121a9e8a2d6",
               "photo2" => "",
               "photo3" => ""
@@ -113,14 +113,14 @@ $array = [
         ],
         [
             "id" => "5a26fd65-ef05-431c-b42e-02f019f9123d",
-            "title" => "Obsession - Dusttale Original",
+            $title => "Obsession - Dusttale Original",
 
-            "category" => "Undertale AU",
-            "price" => 14.99,
-            "sale" => 12.99,
-            "duration" => 3.10,
-            "age" => 8,
-            "photos" => [
+            $category => "Undertale AU",
+            $price => 14.99,
+            $sale => 12.99,
+            $duration => 3.10,
+            $age => 8,
+            $photos => [
                 "photo1" => "https://i.scdn.co/image/ab67616d0000b2736a487b8ee6230dceba4725f2",
                 "photo2" => "",
                 "photo3" => ""
@@ -128,14 +128,14 @@ $array = [
         ],
         [
           "id" => "16d815a0-6fcf-4f96-8b3e-d78eedef7286",
-          "title" => "BIG SHOT - Deltarune Remix",
+          $title => "BIG SHOT - Deltarune Remix",
 
-          "category" => "Deltarune Remix",
-          "price" => 11.99,
-          "sale" => 9.99,
-          "duration" => 3.17,
-          "age" => 9,
-          "photos" => [
+          $category => "Deltarune Remix",
+          $price => 11.99,
+          $sale => 9.99,
+          $duration => 3.17,
+          $age => 9,
+          $photos => [
               "photo1" => "https://i.scdn.co/image/ab67616d0000b27344c914f478ca06b3fe8076a2",
               "photo2" => "",
               "photo3" => ""
@@ -143,72 +143,72 @@ $array = [
         ],
         [
           "id" => "e2b9efa7-64b4-4958-89d3-25b3524bc4de",
-          "title" => "Hysterical - Deltarune Remix",
+          $title => "Hysterical - Deltarune Remix",
 
-          "category" => "Deltarune Remix",
-          "price" => 0.00,
-          "sale" => false,
-          "duration" => 0.00,
-          "age" => 10,
-          "photos" => [
-              "photo1" => "",
+          $category => "Deltarune Remix",
+          $price => 10.99,
+          $sale => false,
+          $duration => 3.25,
+          $age => 10,
+          $photos => [
+              "photo1" => "https://i.scdn.co/image/ab67616d0000b273bce6b3d91965fe4b8fa47ed6",
               "photo2" => "",
               "photo3" => ""
           ]
       ],
       [
           "id" => "355631ce-9ee6-45a5-b709-ac7c4d3773c3",
-          "title" => "Fun With Electrostatic - Errortale Remix",
+          $title => "Fun With Electrostatic - Errortale Remix",
 
-          "category" => "Undertale AU",
-          "price" => 0.00,
-          "sale" => 0.00,
-          "duration" => 0.00,
-          "age" => 11,
-          "photos" => [
-              "photo1" => "",
+          $category => "Undertale AU",
+          $price => 11.99,
+          $sale => false,
+          $duration => 3.05,
+          $age => 11,
+          $photos => [
+              "photo1" => "https://i.scdn.co/image/ab67616d0000b273a835e1228c922fdcb8fc9feb",
               "photo2" => "",
               "photo3" => ""
           ]
       ],
       [
           "id" => "968ef7e0-2a3a-433c-ace7-8cf6bc6294ea",
-          "title" => "Spear of a True Hero - Undertale Remix",
+          $title => "Spear of a True Hero - Undertale Remix",
 
-          "category" => "Undertale Remix",
-          "price" => 0.00,
-          "sale" => 0.00,
-          "duration" => 0.00,
-          "age" => 12,
-          "photos" => [
-              "photo1" => "",
+          $category => "Undertale Remix",
+          $price => 10.99,
+          $sale => false,
+          $duration => 3.57,
+          $age => 12,
+          $photos => [
+              "photo1" => "https://i.scdn.co/image/ab67616d0000b2731ee6236a101898abb7a2e161",
               "photo2" => "",
               "photo3" => ""
           ]
       ],
       [
         "id" => "4e0d07a3-fb98-49dc-aef4-c14f038cbb21",
-        "title" => "Rewind - Undertale Original",
-        "category" => "Original",
-        "price" => 0.00,
-        "sale" => false,
-        "duration" => 0.00,
-        "age" => 13,
-        "photos" => [
-            "photo1" => "",
+        $title => "Rewind - Undertale Original",
+        $category => "Original",
+        $price => 13.99,
+        $sale => false,
+        $duration => 3.44,
+        $age => 13,
+        $photos => [
+            "photo1" => "https://i.scdn.co/image/ab67616d0000b273eb11675bbb9873e9d96ff650",
             "photo2" => "",
             "photo3" => ""
         ]
       ],
       [
         "id" => "9038e2da-0331-4c0e-8ca1-2db7e7756af2",
-        "title" => "Red Light - Underfell Original",
-        "category" => "Original",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 14,
-        "photos" => [
+        $title => "Red Light - Underfell Original",
+        $category => "Original",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 14,
+        $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b27389b5314333251f420d1c09a0",
             "photo2" => "",
             "photo3" => ""
@@ -216,13 +216,13 @@ $array = [
       ],
       [
         "id" => "b65b3234-6981-4c59-bf0f-43239474f0f9",
-        "title" => "Hello World - Aftertale Original",
-        "category" => "Original",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 15,
-        "photos" => [
+        $title => "Hello World - Aftertale Original",
+        $category => "Original",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 15,
+        $photos => [
           "photo1" => "https://i.scdn.co/image/ab67616d0000b2731fc2f5ab28c832a1094df62b",
           "photo2" => "",
           "photo3" => ""
@@ -230,14 +230,14 @@ $array = [
       ],
       [
         "id" => "b65b3234-6981-4c59-bf0f-43239474f0f9",
-        "title" => "Hello World - Aftertale Original",
+        $title => "Hello World - Aftertale Original",
   
-          "category" => "Original",
-          "price" => 0.00, 
-          "sale" => 0.00,
-          "duration" => 0.00,
-          "age" => 16,
-          "photos" => [
+          $category => "Original",
+          $price => 0.00, 
+          $sale => 0.00,
+          $duration => 0.00,
+          $age => 16,
+          $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b2731fc2f5ab28c832a1094df62b",
             "photo2" => "",
             "photo3" => ""
@@ -245,13 +245,13 @@ $array = [
       ],
       [
         "id" => "9f153ce0-9776-4884-9330-568c4680e21f",
-        "title" => "Pandemic - Underfresh Original",
-        "category" => "Original",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 17,
-        "photos" => [
+        $title => "Pandemic - Underfresh Original",
+        $category => "Original",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 17,
+        $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b2731f13ee83ecc3a4bc08dce62b",
             "photo2" => "",
             "photo3" => ""
@@ -259,13 +259,13 @@ $array = [
       ],
       [
         "id" => "492dc956-36b1-47c8-9248-fce79d79eccf",
-        "title" => "Dark Darker Yet Monster - Undertale Remix",
-        "category" => "Undertale Remix",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 18,
-        "photos" => [
+        $title => "Dark Darker Yet Monster - Undertale Remix",
+        $category => "Undertale Remix",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 18,
+        $photos => [
           "photo1" => "https://i.scdn.co/image/ab67616d0000b2734bd80a0cd131f21debd0d07d",
           "photo2" => "",
           "photo3" => ""
@@ -273,13 +273,13 @@ $array = [
       ],
       [
         "id" => "257cc213-051a-44e4-8fb0-06639b8f84ac",
-        "title" => "Tokyovania[Sans & Papyrus Vocals] - Undertale Remix",
-          "category" => "Undertale Remix",
-          "price" => 0.00, 
-          "sale" => 0.00,
-          "duration" => 0.00,
-          "age" => 19,
-          "photos" => [
+        $title => "Tokyovania[Sans & Papyrus Vocals] - Undertale Remix",
+          $category => "Undertale Remix",
+          $price => 0.00, 
+          $sale => 0.00,
+          $duration => 0.00,
+          $age => 19,
+          $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b27363bbb4ed7019cd5d3c515d08",
             "photo2" => "",
             "photo3" => ""
@@ -287,13 +287,13 @@ $array = [
       ],
       [
         "id" => "79da8bfe-a88b-43f4-8647-9a15be961c4a",
-        "title" => "Megalotrousle - Undertale Remix",
-        "category" => "Undertale Remix",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 20,
-        "photos" => [
+        $title => "Megalotrousle - Undertale Remix",
+        $category => "Undertale Remix",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 20,
+        $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b273d887b8e4146632d47ab0c6ee",
             "photo2" => "",
             "photo3" => ""
@@ -301,13 +301,13 @@ $array = [
       ],
       [
         "id" => "8124c7af-7cbf-45a4-9062-832e2005a0fa",
-        "title" => "Darker yet Darker - Undertale Remix",
-        "category" => "Undertale Remix",
-        "price" => 0.00,
-        "sale" => 0.00,
-        "duration" => 0.00,
-        "age" => 21
-        "photos" => [
+        $title => "Darker yet Darker - Undertale Remix",
+        $category => "Undertale Remix",
+        $price => 0.00,
+        $sale => 0.00,
+        $duration => 0.00,
+        $age => 21
+        $photos => [
           "photo1" => "https://i.scdn.co/image/ab67616d0000b273b46c6f2180b6a41487fed6d0",
           "photo2" => "",
           "photo3" => ""
@@ -315,13 +315,13 @@ $array = [
       ],
       [
         "id" => "9ef0dffd-6c82-4647-8ba1-6876911a817d",
-        "title" => "Battle Against a True Hero - Undertale Remix",
-          "category" => "Undertale Remix",
-          "price" => 0.00, 
-          "sale" => 0.00,
-          "duration" => 0.00,
-          "age" => 22,
-          "photos" => [
+        $title => "Battle Against a True Hero - Undertale Remix",
+          $category => "Undertale Remix",
+          $price => 0.00, 
+          $sale => 0.00,
+          $duration => 0.00,
+          $age => 22,
+          $photos => [
             "photo1" => "https://i.scdn.co/image/ab67616d0000b2734ae9002283502758b498d615",
             "photo2" => "",
             "photo3" => ""
@@ -333,7 +333,10 @@ $array = [
     //end of array
 ];
 
+foreach($products as $x){
 
+  debug_to_console($_GET["id"]);
+}
 
 ?>
 
